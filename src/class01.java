6c4ff56d0c7a8b3ea02ac589b42b49c9efe72497
manikmatta <manikmatta17@yahoo.com>
2019-09-04 21:09:59 +1000

    learning java

--- src/class01.java
+++ src/class01.java
@@ -1,13 +1,22 @@
 
-public class class01 implements learningInterface {
+public class class01 implements learningInterface, learningInterface1 {
 
 	@Override
 	public void taxslab() {
+		System.out.println("tax slab of interface1");
 
 	}
 
 	@Override
 	public void test() {
+		System.out.println("test");
+
+	}
+
+	@Override
+	public void test1() {
+		System.out.println("test1");
+		// TODO Auto-generated method stub
 
 	}
 
---
+++ src/driver.java
@@ -0,0 +1,12 @@
+
+public class driver {
+
+	public static void main(String[] args) {
+		class01 cl = new class01();
+		
+		cl.taxslab();
+		cl.test();
+
+	}
+
+}
